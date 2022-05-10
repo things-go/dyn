c0a47c03856a6f5fbdcc68698bc2907ffdfa3010
thinkgo <thinkgo@aliyun.com>
2022-05-10 17:09:34 +0800

    fix: move binding to binding path

--- binding/binding.go
+++ binding/binding.go
@@ -1,4 +1,4 @@
-package ginp
+package binding
 
 import (
 	"context"
--- ginp/implemented.go
+++ ginp/implemented.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/things-go/dyn/binding"
 	"github.com/things-go/dyn/errors"
 )
 
 type Implemented struct{}
 
 func (*Implemented) Validate(ctx context.Context, v interface{}) error {
-	return Validate(ctx, v)
+	return binding.Validate(ctx, v)
 }
 
 func (*Implemented) ErrorEncoder(c *gin.Context, err error, isBadRequest bool) {
