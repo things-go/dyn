842e73a5eddfdcdc13ee48ed78f4c17fbb733259
thinkgo <thinkgo@aliyun.com>
2022-05-31 10:10:59 +0800

    fix(gctx): rename gctx to propagate

--- propagate/gin.go
+++ propagate/gin.go
@@ -1,4 +1,4 @@
-package gctx
+package propagate
 
 import (
 	"context"
---
+++ propagate/propagate.go
@@ -0,0 +1,47 @@
+package propagate
+
+import (
+	"context"
+)
+
+type Propagator interface {
+	// Kind transporter
+	// grpc
+	// http
+	Kind() Kind
+}
+
+// Kind defines the type of Transport
+type Kind string
+
+func (k Kind) String() string { return string(k) }
+
+// Defines a set of transport kind
+const (
+	KindGRPC Kind = "grpc"
+	KindHTTP Kind = "http"
+)
+
+type (
+	ctxPropagatorKey struct{}
+)
+
+// WithValuePropagator returns a new Context that carries value.
+func WithValuePropagator(ctx context.Context, p Propagator) context.Context {
+	return context.WithValue(ctx, ctxPropagatorKey{}, p)
+}
+
+// FromPropagator returns the Propagator value stored in ctx, if any.
+func FromPropagator(ctx context.Context) (p Propagator, ok bool) {
+	p, ok = ctx.Value(ctxPropagatorKey{}).(Propagator)
+	return
+}
+
+// MustFromPropagator returns the Propagator value stored in ctx.
+func MustFromPropagator(ctx context.Context) Propagator {
+	p, ok := ctx.Value(ctxPropagatorKey{}).(Propagator)
+	if !ok {
+		panic("propagate: must be set Propagator into context but it is not!!!")
+	}
+	return p
+}
