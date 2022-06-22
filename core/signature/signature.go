a204d9c707d1da4a36345b9b6f34377fd8609d57
thinkgo <thinkgo@aliyun.com>
2022-06-22 09:21:34 +0800

    feat(signature): add signature

---
+++ core/signature/signature.go
@@ -0,0 +1,121 @@
+package signature
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"unsafe"
+)
+
+// error defined
+var (
+	ErrInputInvalidLength     = errors.New("encoded message length must be more than zero")
+	ErrInputNotMoreABlock     = errors.New("decoded message length must be more than a block size")
+	ErrInputNotMultipleBlocks = errors.New("decoded message length must be multiple of block size")
+	ErrInvalidIvSize          = errors.New("iv length must equal block size")
+	ErrUnPaddingOutOfRange    = errors.New("unPadding out of range")
+)
+
+// Signature hmac sha256的base64
+func Signature(key, str string) string {
+	h := hmac.New(sha256.New, []byte(key))
+	h.Write([]byte(str))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+// RsaEncrypt rsa PKCS #1 v1.5. and base64 encoded.
+func RsaEncrypt(pub *rsa.PublicKey, rawText string) (string, error) {
+	b, err := rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(rawText))
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(b), nil
+}
+
+// RsaDecrypt  base64 decoded and rsa PKCS #1 v1.5.
+func RsaDecrypt(pri *rsa.PrivateKey, ciphertext string) (string, error) {
+	b, err := base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+	bb, err := rsa.DecryptPKCS1v15(rand.Reader, pri, b)
+	if err != nil {
+		return "", err
+	}
+	return *(*string)(unsafe.Pointer(&bb)), nil
+}
+
+// Encrypt aes cbc, iv + ciphertext base64 encoded.
+// key must 16, 24, 32
+func Encrypt(key, rawText string) (string, error) {
+	bsKey := []byte(key)
+	cip, err := aes.NewCipher(bsKey)
+	if err != nil {
+		return "", err
+	}
+	blockSize := cip.BlockSize()
+
+	orig := PCKSPadding([]byte(rawText), blockSize)
+	cipherText := make([]byte, blockSize+len(orig))
+
+	// 生成随机iv
+	_, err = rand.Read(cipherText[:blockSize])
+	if err != nil {
+		return "", err
+	}
+	iv := cipherText[:blockSize]
+	cipher.NewCBCEncrypter(cip, iv).CryptBlocks(cipherText[blockSize:], orig)
+	// iv + ciphertext 一起进行 sha256
+	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+// Decrypt aes cbc,  base64 decoded iv + ciphertext.
+// key must 16, 24, 32
+func Decrypt(key, cipherText string) ([]byte, error) {
+	body, err := base64.StdEncoding.DecodeString(cipherText)
+	if err != nil {
+		return nil, err
+	}
+	cip, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+	blockSize := cip.BlockSize()
+	if len(body) == 0 || len(body)%blockSize != 0 {
+		return nil, ErrInputNotMultipleBlocks
+	}
+	iv, msg := body[:blockSize], body[blockSize:]
+	cipher.NewCBCDecrypter(cip, iv).CryptBlocks(msg, msg)
+	return PCKSUnPadding(msg, blockSize)
+}
+
+// PCKSPadding PKCS#5和PKCS#7 填充
+func PCKSPadding(origData []byte, blockSize int) []byte {
+	padSize := blockSize - len(origData)%blockSize
+	padText := bytes.Repeat([]byte{byte(padSize)}, padSize)
+	return append(origData, padText...)
+}
+
+// PCKSUnPadding PKCS#5和PKCS#7 解填充
+func PCKSUnPadding(origData []byte, blockSize int) ([]byte, error) {
+	orgLen := len(origData)
+	if orgLen == 0 {
+		return nil, ErrUnPaddingOutOfRange
+	}
+	unPadSize := int(origData[orgLen-1])
+	if unPadSize > blockSize || unPadSize > orgLen {
+		return nil, ErrUnPaddingOutOfRange
+	}
+	for _, v := range origData[orgLen-unPadSize:] {
+		if v != byte(unPadSize) {
+			return nil, ErrUnPaddingOutOfRange
+		}
+	}
+	return origData[:(orgLen - unPadSize)], nil
+}
---
+++ core/signature/signature_test.go
@@ -0,0 +1,98 @@
+package signature
+
+import (
+	"crypto/rand"
+	"io"
+	"testing"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/stretchr/testify/require"
+)
+
+var aesKeySizes = []int{16, 24, 32}
+
+func TestEncryptDecrypt(t *testing.T) {
+	plainText := "helloworld,this is golang language. welcome"
+	for _, keySize := range aesKeySizes {
+		key := make([]byte, keySize)
+		_, err := io.ReadFull(rand.Reader, key)
+		require.NoError(t, err)
+
+		cipherText, err := Encrypt(string(key), plainText)
+		require.NoError(t, err)
+
+		got, err := Decrypt(string(key), cipherText)
+		require.NoError(t, err)
+		require.Equal(t, []byte(plainText), got)
+	}
+}
+
+var pri = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAuC0yzDEWZ/ZF492cxgpSbcki2HCuhJACWi6I2m/AZHiTPU95
+IiPDbI23PtvAD7pq9nMypFM3d/t5WiNRBQht+W+eDUSxFdWkHlUTZv5xNZn4H0ZS
+4rO8/69DwSjrhbO3Gh/wykyajPUvkU1ispJ0jqnnLtVhp8zWPfktBKodMQMLSxSY
+V9CHDBriUfvI9eUcFDtiL1IA36u/0s8sO1ril9jp/ap2gbLKNRIp/TtiO2p5oS73
+sJZJyA0aBhcr8zIHm1vp42KIqyKMwDlF24OEdP5Xhgme0konTglTU4OPzbzRPTzg
+FWeWgr1BoQqtjQ36kzpJgpFIbZq89sQGa6NcNwIDAQABAoIBAFJkNeNO2l0ruNxV
+GXsmxvVCE4qL7iZzXfoF80b00zafEg9NbD0vjo8TXrSKDgN7O7qAklkcuSW9o+4E
++our17bMjgIVhrrj1gWTvZhHn1GBTZSAGzg7fANN3pucuLAZU4ImY8u8NS91yA/S
+dKK6DdZ8f2VeI8+zPOyAefkqOODhX1qayqTDUClYKTP5FnuFx+L2AXNccyBogegm
+ZNCJ2nUVBbLWikIAZGaJ3dPS6v69S0jB5xPTN0tj7m7DSv0QkzhM1xucmcZZmLMG
+yd/JtB/87Rgr4jyTxd289rXg1pG8PwAy9VQYsZ78wnThRD39f/kk88d0M34gdn+y
+FM4inpECgYEA4JtMJWUY7IyNsww7sFMIc6IEnLDqZD8Tf6fhk7WfiEDUnRxL8TWH
+pySwbFBA7JeWqhJJ4Tj8bxb/3epjrA5QKLuKz4oaei4vChIhD3eLppE9+WibA/67
+RMv54OuYhey9uq/FXVYHQ5TlZq3lMOnqhOT2y6wrm799u4h8e2G0uLMCgYEA0etD
+VHpXKTkeIN0fepWoOEDGdX+Kbh0zhHZeWa4RLZt2I6Hi/ZUkveOIYoSdARcjM09r
+6KUXX5W6UZ4NbBZO7cNeMnZPAzLA6LcBahzlDyPRBL5nVhpSJGuTzNfLWwcPZIqe
+4MNFPYEBmSrSswIXPEWhdJz1lZuVsQtKUMK4aG0CgYBuotCdUvE2A/4AhjQYpK3z
+F4miDVtHyfI23WE2Oy68FQMl6LxXsoCBiocEs3tnjzv9xkhyEnn11qRukXhLVjmR
+9t9nX6WvLXSqR0fVsJMvlzep1ScWjrF8L+WEL0jQH09N2Csl0Kx/U6a0L1BICdEl
+aQtQRByu+WJbr91xgS1eFQKBgBEFZIY7DUo4aWr8wwqri+JTzkPEvbLEB2NcPbZD
+2Py7uE6XV9J7/2iuRGbInfpyp9YHQJaynDyR5XOsvyXegTPiPYcV9L4rpVy5ShIS
+mbgqjU43KiXfKH3vgyJ+9OxCnErouo07CCg+h6SlxkPhjYTDmJ3eBEPHQ9IBOltm
+DpHZAoGAKCOX7ub+E2wx2cqrjQZ7KCGOiEEZEdsRulblxks8VZa4tVbDCoUj3X16
+XyvtdW6U02yy4rls18un3yrhJk7HE/21NvQPeSEJcS0bl+//61nNaUBzqbjWM4Ii
+vrdhOYn2ywSTuU0Frna2ODulw/d3gS+SqKlSfoVvuG/+xQwrp9M=
+-----END RSA PRIVATE KEY-----
+`
+
+var pub = `
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuC0yzDEWZ/ZF492cxgpS
+bcki2HCuhJACWi6I2m/AZHiTPU95IiPDbI23PtvAD7pq9nMypFM3d/t5WiNRBQht
++W+eDUSxFdWkHlUTZv5xNZn4H0ZS4rO8/69DwSjrhbO3Gh/wykyajPUvkU1ispJ0
+jqnnLtVhp8zWPfktBKodMQMLSxSYV9CHDBriUfvI9eUcFDtiL1IA36u/0s8sO1ri
+l9jp/ap2gbLKNRIp/TtiO2p5oS73sJZJyA0aBhcr8zIHm1vp42KIqyKMwDlF24OE
+dP5Xhgme0konTglTU4OPzbzRPTzgFWeWgr1BoQqtjQ36kzpJgpFIbZq89sQGa6Nc
+NwIDAQAB
+-----END PUBLIC KEY-----
+
+`
+
+func TestRsaEncryptDecrypt(t *testing.T) {
+	priKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(pri))
+	require.NoError(t, err)
+	pubKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(pub))
+	require.NoError(t, err)
+
+	want := "helloworld,this is golang language. welcome"
+
+	mid, err := RsaEncrypt(pubKey, want)
+	require.NoError(t, err)
+
+	got, err := RsaDecrypt(priKey, mid)
+	require.NoError(t, err)
+
+	require.Equal(t, want, got)
+}
+
+func TestPCKS(t *testing.T) {
+	want := []byte("1234567890abcdef")
+
+	b := PCKSPadding(want, 16)
+
+	got, err := PCKSUnPadding(b, 16)
+	require.NoError(t, err)
+	require.Equal(t, want, got)
+}
