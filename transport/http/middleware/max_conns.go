3184c0635d3ac4f9cf855815cf0ce8cc273e3be2
thinkgo <thinkgo@aliyun.com>
2022-07-21 09:19:10 +0800

    feat: add limit, add http middleware

---
+++ transport/http/middleware/max_conns.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/things-go/dyn/core/limit"
+	"github.com/things-go/dyn/log"
+)
+
+// MaxConns returns a middleware that limit the concurrent connections.
+func MaxConns(n int) gin.HandlerFunc {
+	if n <= 0 {
+		return func(c *gin.Context) {
+			c.Next()
+		}
+	}
+	latch := limit.NewLimit(n)
+	return func(c *gin.Context) {
+		if latch.TryBorrow() {
+			defer func() {
+				if err := latch.Return(); err != nil {
+					log.WithContext(c.Request.Context()).Error(err)
+				}
+			}()
+			c.Next()
+		} else {
+			c.AbortWithStatus(http.StatusServiceUnavailable)
+		}
+	}
+}
