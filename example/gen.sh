07060418f06fe05dcb893255d5cd326cc08f3e82
thinkgo <thinkgo@aliyun.com>
2022-12-20 13:42:26 +0800

    fix: add proto-gen-go-enum command

--- example/gen.sh
+++ example/gen.sh
@@ -8,6 +8,7 @@ thirdPartyDir=${projDir}/example/third_party
 protoc \
   -I ${projDir}/${protoDir} \
   -I ${thirdPartyDir} \
+  -I ${projDir} \
   --go_out=${outDir} \
   --go_opt paths=source_relative \
   --go-gin_out ${outDir} \
@@ -15,4 +16,24 @@ protoc \
   --go-gin_opt rpc_mode=official \
   --go-grpc_out ${outDir} \
   --go-grpc_opt paths=source_relative \
-  hello.proto
+  --go-enum_out ${outDir} \
+  --go-enum_opt paths=source_relative \
+  --go-enum_opt merge=true \
+  --go-enum_opt filename=typing \
+  --go-enum_opt package=examples \
+  --go-enum_opt go_package="github.com/things-go/examples" \
+  hello.proto enum.proto
+
+protoc \
+  -I ${projDir}/${protoDir} \
+  -I ${thirdPartyDir} \
+  -I ${projDir} \
+  --go-enum_out ${outDir} \
+  --go-enum_opt suffix=".validate.pb.go" \
+  --go-enum_opt template=${projDir}/${protoDir}/custom_template.tpl \
+  --go-enum_opt paths=source_relative \
+  --go-enum_opt merge=true \
+  --go-enum_opt filename=typing \
+  --go-enum_opt package=examples \
+  --go-enum_opt go_package="github.com/things-go/examples" \
+  hello.proto enum.proto
