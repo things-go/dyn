2e668c379d7fc93770f53408019e744f3dd5c135
thinkgos <thinkgo@aliyun.com>
2024-04-27 11:56:40 +0800

    fix: fix deploy

--- pkg/deploy/default.go
+++ pkg/deploy/default.go
@@ -1,44 +1,6 @@
-//go:generate stringer -type=Mode -linecomment
 package deploy
 
-import (
-	"strings"
-)
-
 var _mode = None
-
-type Mode int
-
-const (
-	None    Mode = iota // none
-	Dev                 // dev
-	Test                // test
-	Uat                 // uat
-	Prod                // prod
-	_maxLen = Prod
-)
-
-// Is is mode equal target.
-func (d Mode) Is(target Mode) bool { return d == target }
-
-// Valid return true if one of dev, test, uat or prod.
-func (d Mode) Valid() bool { return d > None && d <= _maxLen }
-
-// Parse m to Mode
-func Parse(s string) Mode {
-	switch strings.ToLower(s) {
-	case Dev.String():
-		return Dev
-	case Test.String():
-		return Test
-	case Uat.String():
-		return Uat
-	case Prod.String():
-		return Prod
-	default:
-		return None
-	}
-}
 
 // Get is current mode equal target.
 func Is(target Mode) bool { return _mode.Is(target) }
